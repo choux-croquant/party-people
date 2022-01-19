5add4260c4b8ca1780343c01d04088acff479eb9
sunminky <suns1502@gmail.com>
2022-01-19 15:52:12 +0900

    🔧 파티룸 생성 컨트롤러 구현

--- backend/src/main/java/com/ssafy/api/controller/RoomController.java
+++ backend/src/main/java/com/ssafy/api/controller/RoomController.java
@@ -1,7 +1,9 @@
 package com.ssafy.api.controller;
 
+import com.ssafy.api.request.RoomCreatePostReq;
 import com.ssafy.api.response.UserLoginPostRes;
 import com.ssafy.api.response.UserRes;
+import com.ssafy.api.service.RoomService;
 import com.ssafy.api.service.UserService;
 import com.ssafy.common.auth.SsafyUserDetails;
 import com.ssafy.common.model.response.BaseResponseBody;
@@ -21,38 +23,42 @@ import springfox.documentation.annotations.ApiIgnore;
 @RestController
 @RequestMapping("/api/v1/rooms")
 public class RoomController {
-	
-	@Autowired
-	UserService userService;
+    @Autowired
+    private RoomService roomService;
 
-	@GetMapping("/users/{room_id}")
-	@ApiOperation(value = "파티룸 접속 사용자 리스트 조회", notes = "로그인한 회원 본인의 정보를 응답한다.")
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/users/{room_id}")
+    @ApiOperation(value = "파티룸 접속 사용자 리스트 조회", notes = "로그인한 회원 본인의 정보를 응답한다.")
     @ApiResponses({
-        @ApiResponse(code = 200, message = "성공"),
-        @ApiResponse(code = 401, message = "인증 실패"),
-        @ApiResponse(code = 404, message = "사용자 없음"),
-        @ApiResponse(code = 500, message = "서버 오류")
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "인증 실패"),
+            @ApiResponse(code = 404, message = "사용자 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
     })
-	public ResponseEntity<UserRes> getRoomUserList(@ApiIgnore Authentication authentication) {
-		/**
-		 * 요청 헤더 액세스 토큰이 포함된 경우에만 실행되는 인증 처리이후, 리턴되는 인증 정보 객체(authentication) 통해서 요청한 유저 식별.
-		 * 액세스 토큰이 없이 요청하는 경우, 403 에러({"error": "Forbidden", "message": "Access Denied"}) 발생.
-		 */
-		SsafyUserDetails userDetails = (SsafyUserDetails) authentication.getDetails();
-		String userId = userDetails.getUsername();
-		User user = userService.getUserByUserid(userId);
+    public ResponseEntity<UserRes> getRoomUserList(@ApiIgnore Authentication authentication) {
+        /**
+         * 요청 헤더 액세스 토큰이 포함된 경우에만 실행되는 인증 처리이후, 리턴되는 인증 정보 객체(authentication) 통해서 요청한 유저 식별.
+         * 액세스 토큰이 없이 요청하는 경우, 403 에러({"error": "Forbidden", "message": "Access Denied"}) 발생.
+         */
+        SsafyUserDetails userDetails = (SsafyUserDetails) authentication.getDetails();
+        String userId = userDetails.getUsername();
+        User user = userService.getUserByUserid(userId);
 
-		return ResponseEntity.status(200).body(UserRes.of(user));
-	}
+        return ResponseEntity.status(200).body(UserRes.of(user));
+    }
 
-	@PostMapping("/")
-	@ApiOperation(value = "파티룸 생성", notes = "<strong>파티룸</strong>을 생성한다.")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
-			@ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
-			@ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
-	})
-	public ResponseEntity<UserLoginPostRes> createPartyRoom(@RequestBody @ApiParam(value = "파티룸 정보", required = true) Room room) {
-		return null;
-	}
+    @PostMapping("/")
+    @ApiOperation(value = "파티룸 생성", notes = "<strong>파티룸</strong>을 생성한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
+            @ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
+            @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
+    })
+    public ResponseEntity<UserLoginPostRes> createPartyRoom(@RequestBody @ApiParam(value = "파티룸 정보", required = true) RoomCreatePostReq roomCreatePostReq) {
+        roomService.createRoom(roomCreatePostReq);
+
+        return ResponseEntity.status(200).body(null);
+    }
 }
