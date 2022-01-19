c6afca0ed0c826c8781d640b9a133b94be66e783
by-gramm <ready2start@naver.com>
2022-01-19 17:37:24 +0900

    :art: Update conference card template

--- frontend/src/views/home/components/conference.vue
+++ frontend/src/views/home/components/conference.vue
@@ -1,69 +1,78 @@
 // 파티 룸 카드 컴포넌트
+
 <template>
-  <el-card :body-style="{ padding: '0px' }">
-    <div class="image-wrapper">
-      <el-skeleton style="width: 100%">
-        <template #template>
-          <el-skeleton-item variant="image" style="width: 100%; height: 190px" />
-        </template>
-      </el-skeleton>
+  <div class="max-w-sm rounded-xl overflow-hidden shadow-lg conference-card">
+    <img class="w-full" :src="thumbnailUrl" alt="PartyRoom Thumbnail">
+    <div class="px-6 py-4">
+      <div class="flex flex-row justify-between">
+        <span class="font-bold text-xl mb-2">{{ title }}</span>
+        <div class="inline">
+          <span>lock<i class="fas fa-lock"></i></span>
+          <span class="rounded-full main-bg-color ml-3 px-3 py-1 text-white">{{currentUserCount}} / {{maxUserCount}}</span>
+        </div>
       </div>
-    <div style="text-align: left; padding: 14px;">
-      <span class="title">{{ title }}</span>
-      <div class="bottom">
-        <span>{{ desc }}</span>
-      </div>
+      <p class="text-gray-700 text-base line-clamp-3 card-description">{{ desc }}</p>
     </div>
-  </el-card>
+    <div class="px-6 pt-4 pb-2 flex flex-row flex-wrap">
+      <span class="inline-block partyroom-tag rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#파티</span>
+      <span class="inline-block partyroom-tag rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#파티</span>
+      <span class="inline-block partyroom-tag rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#파티</span>
+      <span class="inline-block partyroom-tag rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#파티</span>
+      <span class="inline-block partyroom-tag rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#파티</span>
+      <span class="inline-block partyroom-tag rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#파티</span>
+    </div>
+  </div>
 </template>
+
 <style>
-.el-card {
-  margin: 0 8px;
-  margin-bottom: 40px;
-}
-.el-card .image-wrapper {
-  width: 100%;
-  height: 190px;
-}
-.el-card .title {
-  font-weight: bold;
-}
-.el-card .bottom {
-  margin-top: 5px;
-  display:-webkit-box;
-  word-wrap:break-word;
-  -webkit-box-orient:vertical;
-  overflow:hidden;
-  text-overflow:ellipsis;
-}
-/* 테블릿, 모바일의 경우 두 줄 말줄임표시 */
-@media (max-width: 1269px) {
-  .el-card .bottom {
-    -webkit-line-clamp: 2;
-    height:42px;
-  }
-}
-/* 데스크탑의 경우 세 줄 말줄임표시 */
-@media (min-width: 1270px) {
-  .el-card .bottom {
-    -webkit-line-clamp: 3;
-    height:60px;
-  }
+.main-bg-color {
+  background-color: #6667AB;
 }
 
+.partyroom-tag {
+  background-color: #FF95B5;
+}
+
+/* 3줄 넘어가면 말줄임 표시 */
+.card-description {
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+}
+
+/* 테블릿, 모바일은 2줄 넘어가면 말줄임 표시 */
+@media (max-width: 1269px) {
+  .card-description {
+    -webkit-line-clamp: 2;
+  }
+}
 </style>
+
 <script>
 export default {
   name: 'Home',
 
   props: {
+    thumbnailUrl: {
+      type: String,
+      default: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+    },
     title: {
       type: String,
-      default: '제목'
+      default: '파티룸 이름'
     },
     desc: {
       type: String,
       default: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    currentUserCount: {
+      type: Number,
+      default: 1,
+    },
+    maxUserCount: {
+      type: Number,
+      default: 8,
     }
   },
 
