489630dd6f98b920f829f81be9ed2cb4ebec24bd
SMG <zcvn123456@gmail.com>
2022-01-19 14:05:00 +0900

    :building-construction: Update router

--- frontend/src/common/lib/vue-router.js
+++ frontend/src/common/lib/vue-router.js
@@ -1,30 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// import LandingPage from '@views/landing/landing-page'
 import Home from '@/views/home/home'
 import ConferencesDetail from '@/views/conferences/conference-detail'
 
-const fullMenu = require('@/views/main/menu.json')
-function makeRoutesFromMenu () {
-  let routes = Object.keys(fullMenu).map((key) => { 
-    if (key === 'home') {
-      return { path: fullMenu[key].path, name: key, component: Home  }
-    } else if (key === 'history') {
-      return { path: fullMenu[key].path, name: key, component: History }
-    } else { // menu.json 에 들어있는 로그아웃 메뉴
-      return null
-    }
-  })
-  // 로그아웃 파싱한 부분 제거
-  routes = routes.filter(item => item)
-  // menu 자체에는 나오지 않는 페이지 라우터에 추가(방 상세보기)
-  routes.push({
-    path: '/conferences/:conferenceId',
-    name: 'conference-detail',
-    component: ConferencesDetail
-  })
-  return routes
-}
 
-const routes = makeRoutesFromMenu()
+const routes = [
+  // {
+  //   path: '/',
+  //   name: 'LandingPage',
+  //   component: LandingPage
+  // },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/conferences/:conferenceId',
+    name: 'ConferenceDetail',
+    component: ConferencesDetail
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(),
