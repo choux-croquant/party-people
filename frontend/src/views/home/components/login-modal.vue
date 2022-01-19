093bc21e2bb2ef9272fbd5a43c5ed635629bc491
SMG <zcvn123456@gmail.com>
2022-01-19 21:59:42 +0900

    :wrench: Update singup template

--- frontend/src/views/home/components/login-modal.vue
+++ frontend/src/views/home/components/login-modal.vue
@@ -1,33 +1,29 @@
 // 로그인 모달 컴포넌트
 <template>
-  <div class="w-full max-w-xs">
-  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2 text-left" for="username">
-        Username
-      </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" v-model="state.form.id">
+  <div class="flex justify-center">
+    <div class="w-full max-w-xs">
+    <form class="bg-main-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <img class="w-40 h-24 mb-4 rounded mx-auto" alt="Vue logo" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg">
+      <div class="mb-4">
+        <input class="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="유저아이디" v-model="state.form.id">
+      </div>
+      <div class="mb-6">
+        <input class="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="비밀번호" v-model="state.form.password">
+      </div>
+      <div class="flex items-center justify-center">
+        <button class="bg-gradient-to-r from-main-100 to-sub-100 text-white font-bold py-1 px-24 rounded-full focus:outline-none focus:shadow-outline" type="button" >
+          로그인
+        </button>
+        <!-- <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+          Forgot Password? get out
+        </a> -->
+      </div>
+    </form>
+    <p class="text-center text-gray-500 text-xs">
+      &copy;2022 PartyPeople Corp. All rights reserved.
+    </p>
     </div>
-    <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2 text-left" for="password" >
-        Password
-      </label>
-      <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" v-model="state.form.password">
-      <p class="text-red-500 text-xs italic">똑바로 써라.</p>
-    </div>
-    <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" @click="clickLogin" type="button" >
-        Sign In
-      </button>
-      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password? get out
-      </a>
-    </div>
-  </form>
-  <p class="text-center text-gray-500 text-xs">
-    &copy;2022 PartyPeople Corp. All rights reserved.
-  </p>
-</div>
+  </div>
   <!-- <el-dialog custom-class="login-dialog" title="로그인" v-model="state.dialogVisible" @close="handleClose">
     <el-form :model="state.form" :rules="state.rules" ref="loginForm" :label-position="state.form.align">
       <el-form-item prop="id" label="아이디" :label-width="state.formLabelWidth" >
@@ -45,7 +41,6 @@
   </el-dialog> -->
 </template>
 <style>
-
 
 
 </style>
