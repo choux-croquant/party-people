8a777046a9db355ee7d27c3dcb31c7e8d9fc5fc5
sunminky <suns1502@gmail.com>
2022-01-19 15:51:54 +0900

    🔧 파티룸 생성 서비스 구현

---
+++ backend/src/main/java/com/ssafy/api/service/RoomServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ssafy.api.service;
+
+import com.ssafy.api.request.RoomCreatePostReq;
+import com.ssafy.db.entity.Room;
+import com.ssafy.db.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("roomService")
+public class RoomServiceImpl implements RoomService{
+    @Autowired
+    RoomRepository roomRepository;
+
+    @Override
+    public Room createRoom(RoomCreatePostReq roomCreatePostReq) {
+        Room room = new Room();
+
+        room.setTitle(roomCreatePostReq.getTitle());
+        room.setDescription(roomCreatePostReq.getDescription());
+        room.setThumbnailUrl(roomCreatePostReq.getThumbnail_url());
+        room.setCapacity(roomCreatePostReq.getCapacity());
+        room.setPassword(roomCreatePostReq.getPassword());
+        room.setStartTime(roomCreatePostReq.getStart_time());
+        room.setActive(roomCreatePostReq.getIs_active());
+        room.setLocked(roomCreatePostReq.getIs_locked());
+
+        return roomRepository.save(room);
+    }
+}
