04a3d3f60d9878f1150fbc3d46ab182ef88fb20a
sunminky <suns1502@gmail.com>
2022-01-20 12:01:02 +0900

    🔨 [Edit RoomService] Entering Session Table

--- backend/src/main/java/com/ssafy/api/service/RoomService.java
+++ backend/src/main/java/com/ssafy/api/service/RoomService.java
@@ -4,6 +4,7 @@ import com.ssafy.api.request.RoomCreatePostReq;
 import com.ssafy.api.request.RoomHostUpdateReq;
 import com.ssafy.db.entity.Room;
 import com.ssafy.db.entity.User;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -11,8 +12,9 @@ import java.util.List;
  *	유저 관련 비즈니스 로직 처리를 위한 서비스 인터페이스 정의.
  */
 public interface RoomService {
-	Room createRoom(RoomCreatePostReq roomCreatePostReq);
+	Room createRoom(RoomCreatePostReq roomCreatePostReq, User user);
 	List<User>  getRoomUserListByRoomId(Long roomId);
 	List<User> updateRoomHostInfo(List<RoomHostUpdateReq> updateHostReq);
 	void updateSessionEndTime(String userid, Long roomId);
+	Room roomEntry(Room room);
 }
--- backend/src/main/java/com/ssafy/api/service/RoomServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/RoomServiceImpl.java
@@ -3,8 +3,10 @@ package com.ssafy.api.service;
 import com.ssafy.api.request.RoomCreatePostReq;
 import com.ssafy.api.request.RoomHostUpdateReq;
 import com.ssafy.db.entity.Room;
+import com.ssafy.db.entity.Session;
 import com.ssafy.db.entity.User;
 import com.ssafy.db.repository.RoomRepository;
+import com.ssafy.db.repository.SessionRepository;
 import com.ssafy.db.repository.SessionRepositorySupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,11 +19,19 @@ public class RoomServiceImpl implements RoomService{
     @Autowired
     RoomRepository roomRepository;
     @Autowired
-    SessionRepositorySupport sessionRepository;
+    SessionRepository sessionRepository;
+    @Autowired
+    SessionRepositorySupport sessionRepositorySupport;
 
     @Override
-    public Room createRoom(RoomCreatePostReq roomCreatePostReq) {
+    public Room roomEntry(Room room) {
+        return null;
+    }
+
+    @Override
+    public Room createRoom(RoomCreatePostReq roomCreatePostReq, User user) {
         Room room = new Room();
+        Session session = new Session();
 
         room.setTitle(roomCreatePostReq.getTitle());
         room.setDescription(roomCreatePostReq.getDescription());
@@ -31,12 +41,21 @@ public class RoomServiceImpl implements RoomService{
         room.setActive(roomCreatePostReq.getIs_active());
         room.setLocked(roomCreatePostReq.getIs_locked());
 
+        /*session.setUser(user);
+        session.setRoom(room);
+        session.setActive(roomCreatePostReq.getIs_active());
+        session.setHost(true);
+
+        sessionRepository.save(session);
+
+        this.roomEntry();*/
+
         return roomRepository.save(room);
     }
 
     @Override
     public List<User> getRoomUserListByRoomId(Long roomId) {
-        return sessionRepository.findUsersByRoomId(roomId);
+        return sessionRepositorySupport.findUsersByRoomId(roomId);
     }
 
     @Override
