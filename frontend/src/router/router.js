8666d9695beacf944c5c21d4f3bf1ff2f60f10e3
SMG <zcvn123456@gmail.com>
2022-01-20 11:33:35 +0900

    :wrench: Update password-confirm

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,17 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import LandingPage from '@views/landing/landing-page'
+import LandingPage from '@/views/landing/landing-page'
 import Home from '@/views/home/home'
-import LoginModal from '@/views/home/components/login-modal'
+import PasswordConfirm from '@/views/home/components/password-confirm'
+// import LoginModal from '@/views/home/components/login-modal'
 import ConferencesDetail from '@/views/partyroom/partyroom-detail'
-import SignupModal from '@/views/home/components/signup-modal'
+// import SignupModal from '@/views/home/components/signup-modal'
+
 
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'LandingPage',
-  //   component: LandingPage
-  // },
+  {
+    path: '/',
+    name: 'LandingPage',
+    component: LandingPage
+  },
   {
     path: '/home',
     name: 'Home',
@@ -22,15 +24,20 @@ const routes = [
     name: 'ConferenceDetail',
     component: ConferencesDetail
   },
+  // {
+  //   path: '/login-modal',
+  //   name: 'LoginModal',
+  //   component: LoginModal
+  // },
+  // {
+  //   path: '/signup-modal',
+  //   name: 'SignupModal',
+  //   component: SignupModal
+  // },
   {
-    path: '/login-modal',
-    name: 'LoginModal',
-    component: LoginModal
-  },
-  {
-    path: '/signup-modal',
-    name: 'SignupModal',
-    component: SignupModal
+    path: '/password-confirm',
+    name: 'PasswordConfirm',
+    component: PasswordConfirm
   },
 ]
 
