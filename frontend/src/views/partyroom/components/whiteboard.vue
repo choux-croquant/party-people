01b5d7d51fc7a25ee7d73c07351fd362c16de853
by-gramm <ready2start@naver.com>
2022-02-08 16:54:02 +0900

    :wrench: Add reset button

--- frontend/src/views/partyroom/components/whiteboard.vue
+++ frontend/src/views/partyroom/components/whiteboard.vue
@@ -1,11 +1,9 @@
 <template>
-	<div class="flex flex-col items-center">
-		<!-- 캔버스 (그림이 그려지는 곳) -->
-		<canvas id="partypeopleCanvas" class="bg-tc-500 canvas"></canvas>
-		<div class="flex flex-col items-center mt-6">
+	<div class="flex flex-row justify-start">
+		<div class="flex flex-col items-center p-2 shadow-lg rounded-2xl h-700">
 			<!-- 두께 설정 -->
-			<div class="p-4 flex items-center">
-				<span class="mr-3 font-bold">Width</span>
+			<div class="p-4 flex flex-col items-center">
+				<span class="mb-2 font-bold text-lg">Width</span>
 				<input
 					type="range"
 					id="widthRange"
@@ -15,52 +13,48 @@
 					step="0.1"
 				/>
 			</div>
-			<div class="flex items-center justify-evenly p-4">
+			<div class="flex flex-col items-center justify-evenly py-3">
+				<span class="mb-2 font-bold text-lg">Color</span>
 				<!-- 색깔 설정 -->
-				<div class="flex" id="jsColors">
+				<div class="flex flex-col items-center" id="jsColors">
 					<div
-						class="flex w-12 h-12 cursor-pointer rounded-full shadow-lg mr-2 color-set selected"
+						class="flex w-10 h-10 cursor-pointer rounded-full shadow-lg mb-3 color-set selected"
 						style="background-color: #2c2c2c"
 					></div>
-					<!-- <div
-						class="flex w-12 h-12 cursor-pointer rounded-full shadow-lg mr-2 color-set"
-						style="background-color: white"
-					></div> -->
 					<div
-						class="flex w-12 h-12 cursor-pointer rounded-full shadow-lg mr-2 color-set"
+						class="flex w-10 h-10 cursor-pointer rounded-full shadow-lg mb-3 color-set"
 						style="background-color: #ff3b30"
 					></div>
 					<div
-						class="flex w-12 h-12 cursor-pointer rounded-full shadow-lg mr-2 color-set"
+						class="flex w-10 h-10 cursor-pointer rounded-full shadow-lg mb-3 color-set"
 						style="background-color: #ff9500"
 					></div>
 					<div
-						class="flex w-12 h-12 cursor-pointer rounded-full shadow-lg mr-2 color-set"
+						class="flex w-10 h-10 cursor-pointer rounded-full shadow-lg mb-3 color-set"
 						style="background-color: #ffcc00"
 					></div>
 					<div
-						class="flex w-12 h-12 cursor-pointer rounded-full shadow-lg mr-2 color-set"
+						class="flex w-10 h-10 cursor-pointer rounded-full shadow-lg mb-3 color-set"
 						style="background-color: #4cd963"
 					></div>
 					<div
-						class="flex w-12 h-12 cursor-pointer rounded-full shadow-lg mr-2 color-set"
+						class="flex w-10 h-10 cursor-pointer rounded-full shadow-lg mb-3 color-set"
 						style="background-color: #5ac8fa"
 					></div>
 					<div
-						class="flex w-12 h-12 cursor-pointer rounded-full shadow-lg mr-2 color-set"
+						class="flex w-10 h-10 cursor-pointer rounded-full shadow-lg mb-3 color-set"
 						style="background-color: #0579ff"
 					></div>
 					<div
-						class="flex w-12 h-12 cursor-pointer rounded-full shadow-lg mr-2 color-set"
+						class="flex w-10 h-10 cursor-pointer rounded-full shadow-lg mb-3 color-set"
 						style="background-color: #5856d6"
 					></div>
 				</div>
 				<!-- 지우개 -->
-				<div class="w-12 h-12 ml-4 mr-6 p-0 cursor-pointer rounded-full bg-tc-500" id="eraser">
-				<!-- <div class="h-8 w-8  cursor-pointer " > -->
+				<div class="w-10 h-10 p-0 cursor-pointer rounded-full bg-tc-500 flex items-center justify-center" id="eraser">
 					<svg
 						version="1.1"
-						class="h-full w-full erasor-icon"
+						class="h-5/6 w-5/6 erasor-icon"
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 500 500"
 					>
@@ -73,22 +67,37 @@
 						/>
 					</svg>
 				</div>
-				<!-- 화이트보드 닫기 버튼 -->
-				<button
-					class="bg-transparent hover:bg-main-200 text-main-200 font-semibold hover:text-tc-500 py-2 px-4 border border-main-200 hover:border-transparent rounded"
-					@click="closeWhiteboard"
-				>
-					Close
-				</button>
+				<div class="mt-10">
+					<!-- 화이트보드 닫기 버튼 -->
+					<button
+						class="mr-1 bg-transparent hover:bg-main-200 text-main-200 text-md font-semibold hover:text-tc-500 py-2 px-4 border border-main-200 hover:border-transparent rounded"
+						@click="closeWhiteboard"
+					>
+						Close
+					</button>
+					<!-- 초기화 버튼 -->
+					<button
+						class="bg-transparent hover:bg-sub-100 text-sub-100 text-md font-semibold hover:text-tc-500 py-2 px-4 border border-sub-100 hover:border-transparent rounded"
+						@click="resetWhiteboard"
+					>
+						Reset
+					</button>
+				</div>
 			</div>
 		</div>
+
+		<!-- 캔버스 (그림이 그려지는 곳) -->
+		<canvas id="partypeopleCanvas" class="bg-tc-500 canvas h-700"></canvas>
 	</div>
 </template>
 
 <style>
+.h-700 {
+	height: 700px;
+}
+
 .canvas {
 	width: 700px;
-	height: 700px;
 	border-radius: 15px;
 	box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
 }
@@ -245,6 +254,12 @@ export default {
 			emit('close-whiteboard');
 		};
 
+		// 화이트보드 초기화 (자기 화면에만 적용)
+		const resetWhiteboard = () => {
+			console.log('asdf')
+			state.ctx.fillRect(0, 0, 700, 700);
+		};
+
 		return {
 			state,
 			sendSignal,
@@ -252,6 +267,7 @@ export default {
 			sendPaintingState,
 			addPaintingSignal,
 			closeWhiteboard,
+			resetWhiteboard,
 		};
 	},
 };
