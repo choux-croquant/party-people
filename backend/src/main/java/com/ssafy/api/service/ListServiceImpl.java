2fe9037d392b255705693e5f24da45c54d721eeb
hayj04 <hayj04@gmail.com>
2022-02-11 00:43:27 +0900

    🏗️ Change tag search algorithm from java to jpa queryDsl

--- backend/src/main/java/com/ssafy/api/service/ListServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/ListServiceImpl.java
@@ -1,15 +1,11 @@
 package com.ssafy.api.service;
 
 import com.ssafy.db.entity.Room;
-import com.ssafy.db.entity.RoomTag;
-import com.ssafy.db.entity.Tag;
 import com.ssafy.db.repository.*;
-import com.sun.xml.bind.v2.TODO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
 
 /**
  *	미팅룸 리스트, 태그 관련 비즈니스 로직 처리를 위한 서비스 구현 정의.
@@ -19,9 +15,7 @@ public class ListServiceImpl implements ListService {
 	@Autowired
 	private ListRepository listRepository;
 	@Autowired
-	private TagRepository tagRepository;
-	@Autowired
-	private RoomTagRepository roomTagRepository;
+	private RoomTagRepositorySupport roomTagRepositorySupport;
 
 	@Override
 	public Page<Room> getRoomList(Pageable pageable) {
@@ -43,32 +37,7 @@ public class ListServiceImpl implements ListService {
 				break;
 			case "hashtag" :
 				String[] hashtags = word.substring(1).split("#");
-				HashMap<Room, Integer> roomTagMap = new HashMap<>();
-
-				// tag 테이블에서 hashtags에 해당하는 태그 리스트를 모두 가져옴
-				List<Tag> tagList = tagRepository.findByTagNameIn(hashtags);
-
-				for (Tag tag : tagList) {
-					// roomTag 테이블에서 각 hashtag에 해당하는 룸 리스트 모두 가져옴
-					List<RoomTag> searchedRooms = roomTagRepository.findByTag(tag);
-
-					// 맵에 룸과 찾는 해시태그를 몇개 포함하는지 저장
-					for (RoomTag searchedRoom : searchedRooms) {
-						Room curRoom = searchedRoom.getRoom();
-						roomTagMap.computeIfPresent(curRoom, (k, v) -> v+1);
-						roomTagMap.putIfAbsent(curRoom, 1);
-					}
-				}
-
-				// 조회하는 해시태그 개수만큼 해시태그를 포함한 룸만 반환
-				List<Room> roomContainingHashtags = new ArrayList<>();
-				int size = hashtags.length;
-				for (Map.Entry<Room, Integer> entry : roomTagMap.entrySet()) {
-					// 종료된 파티룸 제외
-					if (entry.getValue() == size && entry.getKey().getEndTime() == null) roomContainingHashtags.add(entry.getKey());
-				}
-
-				roomList = new PageImpl<>(roomContainingHashtags, sort, roomContainingHashtags.size());
+				roomList = roomTagRepositorySupport.getRoomTagByTagName(hashtags, sort);
 				break;
 		}
 		return roomList;
