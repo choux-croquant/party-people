b062497861f06c591fda684da08cdd17c38b14ae
hayj04 <hayj04@gmail.com>
2022-02-10 13:37:57 +0900

    🔧 Add jpa RoomTagRepository

---
+++ backend/src/main/java/com/ssafy/db/repository/RoomTagRepository.java
@@ -0,0 +1,13 @@
+package com.ssafy.db.repository;
+
+import com.ssafy.db.entity.RoomTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 태그와 룸 테이블 조인 테이블인 룸 태그 관련 디비 쿼리 생성을 위한 JPA Query Method 인터페이스 정의.
+ */
+@Repository
+public interface RoomTagRepository extends JpaRepository<RoomTag, Long> {
+
+}
