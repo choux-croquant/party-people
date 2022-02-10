7d5906b252afaac236188f7c2eb527261b64d5a9
hayj04 <hayj04@gmail.com>
2022-02-11 00:48:10 +0900

    🔧 Add tag save when create room

--- backend/src/main/java/com/ssafy/api/service/RoomService.java
+++ backend/src/main/java/com/ssafy/api/service/RoomService.java
@@ -4,6 +4,7 @@ import com.ssafy.api.request.RoomCreatePostReq;
 import com.ssafy.api.request.RoomEntryPostReq;
 import com.ssafy.api.request.RoomHostUpdateReq;
 import com.ssafy.db.entity.Room;
+import com.ssafy.db.entity.RoomTag;
 import com.ssafy.db.entity.Session;
 import com.ssafy.db.entity.User;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,6 +19,9 @@ public interface RoomService {
 	Room createRoom(RoomCreatePostReq roomCreatePostReq, MultipartFile multipartFile);
 	Room deleteRoom(long roomId);
 	Room updateThumbnail(long roomId, MultipartFile multipartFile);
+	// tag 테이블에 태그를 저장하고 room_tag 테이블에 반영한다
+	List<RoomTag> saveAndGetRoomTags(Room room, String tagString);
+	List<RoomTag> updateTags(Room room, String[] hashtags);
 	List<User> getRoomUserListByRoomId(Long roomId);
 	List<Session> getSessionsByRoomId(Long roomId);
 	void updateRoomHostInfo(Long roomId, List<RoomHostUpdateReq> updateHostReq);
