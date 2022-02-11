3c2fc551520a941f965d68d8482ea55ce9986249
hayj04 <hayj04@gmail.com>
2022-02-11 16:37:47 +0900

    🐛 Fix room create error without hashtag

--- backend/src/main/java/com/ssafy/api/service/RoomServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/RoomServiceImpl.java
@@ -63,6 +63,7 @@ public class RoomServiceImpl implements RoomService {
     // tag 테이블에 태그를 저장하고 room_tag 테이블에 반영한다
     @Override
     public List<RoomTag> saveAndGetRoomTags(Room room, String tagString) {
+        if (tagString.length() == 0 || tagString == null) return null;
         String[] hashtags = tagString.substring(1).split("#");
         List<RoomTag> roomTagList = updateTags(room, hashtags);
         return roomTagRepository.saveAll(roomTagList);
