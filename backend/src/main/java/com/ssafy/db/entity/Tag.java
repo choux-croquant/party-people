b5558c0eafd02f1cf16498a69044e1b612d63d7c
박건우 <hggygw77@naver.com>
2022-02-11 10:54:51 +0900

    :hammer: Update DB entities

--- backend/src/main/java/com/ssafy/db/entity/Tag.java
+++ backend/src/main/java/com/ssafy/db/entity/Tag.java
@@ -1,6 +1,7 @@
 package com.ssafy.db.entity;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,9 +20,11 @@ import java.util.List;
 public class Tag extends BaseEntity{
     private String tagName;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "tag")
     private List<UserTag> userTags;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "tag")
     private List<RoomTag> roomTags;
 
