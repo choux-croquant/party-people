d2e064e9107c517b054b461d86583af5f9cc59a8
hayj04 <hayj04@gmail.com>
2022-02-17 12:08:48 +0900

    🐛 Fix get subscribers nicknames

--- frontend/src/views/partyroom/components/user-video.vue
+++ frontend/src/views/partyroom/components/user-video.vue
@@ -29,6 +29,7 @@
 }
 </style>
 <script>
+import { computed } from 'vue';
 import OvVideo from './ov-video';
 import { useStore } from 'vuex';
 export default {
@@ -42,28 +43,33 @@ export default {
 		return {
 			store: useStore(),
 			isSpeaking: false,
+			clientData: computed(() => {
+				const { clientData } = this.getConnectionData();
+				this.getSubscriberNickname(clientData);
+				return clientData;
+			}),
+			userNickname: null,
 		};
 	},
 
 	props: {
 		streamManager: Object,
 	},
-	// 유저 아이디 정보와 유저 닉네임 정보를 computed 속성으로 불러온 후 사용
-	computed: {
-		clientData() {
-			const { clientData } = this.getConnectionData();
-			return clientData;
-		},
-		userNickname() {
-			const userNickname = this.store.getters['auth/getUserNickname'];
-			return userNickname;
-		},
-	},
-	// 함수를 통해 css속성을 추가/제거 하는 것으로 speech detection 시 하이라이트 효과 구현
 	methods: {
 		getConnectionData() {
 			const { connection } = this.streamManager.stream;
 			return JSON.parse(connection.data);
+		},
+		getSubscriberNickname(userId) {
+			this.store
+				.dispatch('root/requestNickname', userId)
+				.then(res => {
+					console.log(res);
+					this.userNickname = res.data.nickname;
+				})
+				.catch(err => {
+					console.log(err);
+				});
 		},
 		highlightOn(name) {
 			const { clientData } = this.getConnectionData();
